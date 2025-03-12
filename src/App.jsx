145220429ab749e765efba0e97a1893de1d7b8d4
anushahashmi071 <anushahashmi071@gmail.com>
2025-03-12 10:53:24 +0500

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React, { useState, useEffect } from "react";
+import tasbih from "./assets/tasbih.jpg";
+import incrementIcon from "./assets/plus.png";
+import decrementIcon from "./assets/remove.png";
+import refreshIcon from "./assets/refresh.png";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      alert("Sorry, Negative Numbers are not allowed...!!!");
+    }
+  };
   useEffect(() => {
     const pressKey = (e) => {
       if (e.key === "=" || e.key === "+") {
@@ -19,38 +32,35 @@ function App() {
           }
         });
       }
+      if(e.key === "R" || e.key === "r"){
+        setCount((0))
+      }
     };
     document.addEventListener("keypress", pressKey);
     return () => {
       document.removeEventListener("keypress", pressKey);
     };
   }, []);
-  
-  // const decrement = () => {
-  //   if (count > 0) {
-  //     setCount(count - 1);
-  //   } else {
-  //     alert("Sorry, Negative Numbers are not allowed...!!!");
-  //   }
-  // };
 
   return (
     <div className="bg-color">
-      <img src="/src/assets/tasbih.jpg" className="logo" alt="Tasbih Image" />
+      <img src={tasbih} className="logo" alt="Tasbih Image" />
       <h1>Tally Count</h1>
       <span>
         <em>(Counter)</em>
       </span>
       <p>
-        Count your <b>Azkar</b> here..!!
+        Count your <b>Zikar</b> here..!!
       </p>
       <h2>{count}</h2>
-      <button className="btn increment">&#43;</button>
-      <button onClick={() => setCount - 1} className="btn decrement">
-        &#8722;
+      <button onClick={increment} className="btn increment">
+        <img src={incrementIcon} alt="Increment Icon" />
+      </button>
+      <button onClick={decrement} className="btn decrement">
+        <img src={decrementIcon} alt="Decrement Icon" />
       </button>
       <button onClick={() => setCount(0)} className="btn reset">
-        &#10226;
+        <img src={refreshIcon} alt="Refresh Icon" />
       </button>
     </div>
   );
